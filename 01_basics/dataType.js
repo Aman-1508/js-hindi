39083a833560c8c0d34768f092a54d2a107dd59b
Aman-1508 <aman734876@gmail.com>
2025-02-08 14:47:16 +0000

    added datatype file

---
+++ 01_basics/dataType.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3 ) // we are using nodeJS , not browser
+
+// tc39 documentation and MDN documentation
+
+let name ="Aman"
+let age = 21
+let isLoggedIn= false
+
+// number -> 2 to power 53
+// bigInt
+// String ->
+// boolean -> true/false
+// null ->standalone value
+// undefined ->
+// symbol -> unique
+
+// object
+
+console.log(typeof undefined) //type undefined
+console.log(typeof object) //type object
