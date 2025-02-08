62119313df86734cf13f7031afcd47378befb360
Aman-1508 <aman734876@gmail.com>
2025-02-08 20:49:02 +0000

    data conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,24 @@
+// let score = "33abc"// NaN//type number
+// let score = null// 0
+// let score = undefined//NaN//type number
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber)
+
+
+// let isLoggedIn = "Aman"
+
+// let booleanLog = Boolean(isLoggedIn)
+// console.log(booleanLog);
+
+// 1 = true 0= true
+// "" = false  "name" = true
+
+let someNumber = 45
+
+let booleanLog = String(someNumber)
+console.log(booleanLog);
+
