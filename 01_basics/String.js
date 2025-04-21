d6721ba2f19bb6aa9647250d62e05f4c7094f2c8
Aman-1508 <aman734876@gmail.com>
2025-04-21 13:00:55 +0000

    String and datatype new start

---
+++ 01_basics/String.js
@@ -0,0 +1,21 @@
+const name ="AmanSingh"
+const repocount = 50
+console.log(`My name is ${name} and my repo count is ${repocount}`);
+
+// Declration of string
+
+const gameName = new String('Aman Singh');
+console.log(typeof gameName);
+// console.log(gameName.length);
+// console.log(gameName.toLowerCase());
+console.log(gameName.charAt(8));
+console.log(gameName.indexOf('S'));
+// substring(start , end) // last index is not included
+// slice(start , end)// can give neg value as a input
+// trim() remove starting and ending spaace
+// replace(oldValue , newValue) 
+// includes(element) search
+// spilt()
+
+
+
