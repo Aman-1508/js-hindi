809b5cfcfb2ef14ee04f2cb9578c8f8938aa4de9
Aman-1508 <aman734876@gmail.com>
2025-04-21 13:29:26 +0000

    number and math file added

---
+++ 01_basics/NumberAndMath.js
@@ -0,0 +1,22 @@
+// const Score = 400
+// console.log(Score);
+// const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString().length);
+// toFixed(number) 
+// toPrecision(number)
+// toLocaleString('en-In') change in standard
+
+
+// ******************Maths****************************
+// Math.abs() 
+// Math.round()
+// Math.ceil()
+// Math.floor()
+//console.log(Math.max(3 , 4, 5, 6));
+// Math.min()
+// Math.random() gives value between 0 and 1 use floor for integer
+const max=20
+const min=10
+
+console.log(Math.floor(Math.ramdom()*(max-min+1)+min))
