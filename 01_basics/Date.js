1dbe8833883e3ee7cdc5064ae7b8ce43edc80c34
Aman-1508 <aman734876@gmail.com>
2025-04-22 10:44:46 +0000

    date time

---
+++ 01_basics/Date.js
@@ -0,0 +1,8 @@
+//  let myDate = new Date()
+//  console.log(myDate.toDateString());
+//  console.log(myDate.toLocaleString());
+
+//  let myCreateDate = new Date(2023, 0 , 23)
+ let myCreateDate = new Date(2023, 0 , 23 ,5,3)
+
+ console.log(myCreateDate.toLocaleDateString());
