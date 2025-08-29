09d68497bdc15665a15bb6e7d15ff9d6aa5ec6fa
Aman-1508 <aman734876@gmail.com>
2025-08-29 22:12:33 +0000

    new starting

--- 01_basics/dataType.js
+++ 01_basics/dataType.js
@@ -1,54 +1,61 @@
-// "use strict"; //treat all JS code as newer version
+// // "use strict"; //treat all JS code as newer version
 
-// // alert(3 + 3 ) // we are using nodeJS , not browser
+// // // alert(3 + 3 ) // we are using nodeJS , not browser
 
-// // tc39 documentation and MDN documentation
+// // // tc39 documentation and MDN documentation
 
-// let name ="Aman"
-// let age = 21
-// let isLoggedIn= false
+// // let name ="Aman"
+// // let age = 21
+// // let isLoggedIn= false
 
-// // number -> 2 to power 53
-// // bigInt 
-// // String ->"---"
-// // boolean -> true/false
-// // null ->standalone value
-// // undefined ->
-// // symbol -> unique
+// // // number -> 2 to power 53
+// // // bigInt 
+// // // String ->"---"
+// // // boolean -> true/false
+// // // null ->standalone value
+// // // undefined ->
+// // // symbol -> unique
 
-// // object
+// // // object
 
-// console.log(typeof undefined) //type undefined
-// console.log(typeof null) //type object
+// // console.log(typeof undefined) //type undefined
+// // console.log(typeof null) //type object
 
-// // primitive 
-// // 7 types : string , number , boolean , null , undefined , symbol, BigInt
+// // // primitive 
+// // // 7 types : string , number , boolean , null , undefined , symbol, BigInt
 
-// // Reference (non primitve)
+// // // Reference (non primitve)
 
-// // Array , Objects, Functions // datatype return- objects
-// const heros =["skt","ironman", "captain","panther"]
-// let myobj={
-//     name: "Aman", age: 21, //key value pair
+// // // Array , Objects, Functions // datatype return- objects
+// // const heros =["skt","ironman", "captain","panther"]
+// // let myobj={
+// //     name: "Aman", age: 21, //key value pair
+// // }
+// // const id= symbol('123')
+// // const bigNumber = 12232633742645234529845263n
+// //  const myFunction = function(){
+// //     console.log("HEllo World")
+// //  }
+// //  stack (primitive) , heap (Non primitive)
+//  let myName = "AmanSingh"
+//  let secondName = "NikhilSingh"
+//  console.log(myName);
+//  console.log(secondName);
+// let user ={
+//         email: "aman@gmail.com",
+//         pasword:"1234579"
 // }
-// const id= symbol('123')
-// const bigNumber = 12232633742645234529845263n
-//  const myFunction = function(){
-//     console.log("HEllo World")
-//  }
-//  stack (primitive) , heap (Non primitive)
- let myName = "AmanSingh"
- let secondName = "NikhilSingh"
- console.log(myName);
- console.log(secondName);
-let user ={
-        email: "aman@gmail.com",
-        pasword:"1234579"
-}
-let user1 = user
-user1.email ="yahoo.com"
-console.log(user);
-console.log(user1);
+// let user1 = user
+// user1.email ="yahoo.com"
+// console.log(user);
+// console.log(user1);
+
+ // Stack (primitive) , heap (Non-Primitive)
+ let my_yt_name = "aman"
+ let an = my_yt_name
+ an = "changed"
+ console.log(my_yt_name);
+ console.log(an);
 
 
  
