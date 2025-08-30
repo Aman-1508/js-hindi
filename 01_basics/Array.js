1dca240fcb396617576aca19c6ef03ba7cbd0699
Aman-1508 <aman734876@gmail.com>
2025-08-30 21:16:30 +0000

    fir se kuch pdha

--- 01_basics/Array.js
+++ 01_basics/Array.js
@@ -1,23 +1,32 @@
-// //Array 
-// let myArray=[0, 1, 2, 3, 4, 5]
-// const myArr2 = new Array(1,2,3,4) 
-// console.log(myArr2[2])
+// // //Array 
+// // let myArray=[0, 1, 2, 3, 4, 5]
+// // const myArr2 = new Array(1,2,3,4) 
+// // console.log(myArr2[2])
 
-//Array methods
-// push(element), pop()
-//unshift(value) , shift() 
-//includes(element) , indexOf(index)
-// join() //join the elemments of  Array in a single string seperated by commas
-// slice(starting , ending(not included)) cut the array in given limit
-// splice(starting , ending(included)) // ivide the array into two group changes the original array
+// //Array methods
+// // push(element), pop()
+// //unshift(value) , shift() 
+// //includes(element) , indexOf(index)
+// // join() //join the elemments of  Array in a single string seperated by commas
+// // slice(starting , ending(not included)) cut the array in given limit
+// // splice(starting , ending(included)) // divide the array into two group changes the original array
 
-const marvel_heroes =["thor", "Ironman" ,"spiderman"]
-const Dc_heroes =["Superman", "flash" ,"batman"]
-const newArray= marvel_heroes.concat(Dc_heroes)
-console.log(newArray);
+// // const marvel_heroes =["thor", "Ironman" ,"spiderman"]
+// // const Dc_heroes =["Superman", "flash" ,"batman"]
+// // const newArray= marvel_heroes.concat(Dc_heroes)
+// // console.log(newArray);
 
-const all_new_heroes = [...marvel_heroes, ...Dc_heroes]
-console.log(all_new_heroes);
-// flat(depth) //seperates the elements inside a array of array
-//isArray()//checks the value for array
-// from() and of()
+// // const all_new_heroes = [...marvel_heroes, ...Dc_heroes]
+// // console.log(all_new_heroes);
+// let empArray = ["Aman" ,78,10.3];
+// empArray[0]="z"
+// console.log(empArray[0]);
+// // flat(depth) //seperates the elements inside a array of array
+// //isArray()//checks the value for array
+// // from() and of()
+let start = ["jan","july","mar","aug"];
+start.shift();
+start.shift();
+start.unshift("june");
+start.unshift("july");
+console.log(start);
