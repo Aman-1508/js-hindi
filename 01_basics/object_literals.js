fa7cd54c86858c1bf94e1009a624ea5a93ba294d
Aman-1508 <aman734876@gmail.com>
2025-09-01 18:34:52 +0000

    objext literals padhe

---
+++ 01_basics/object_literals.js
@@ -0,0 +1,73 @@
+// // const student={
+// //     name : "Aman Singh" ,
+// //     roll_No : 088,
+// //     marks : 95.5
+// // };
+// // console.log(student);
+
+// // const item ={
+// //     price : "@50RS",
+// //     Discount : "50%",
+// //     colors : ["red","yellow"]
+// // };
+// // console.log(item);
+
+// const information = {
+//     username :"Aman015",
+//     content :"THis is my kingdom",
+//     1 : 1500,
+//     comment : 200,
+//     tagged : ["keshav" , "Amarnath" ,"Ashutosh"]
+// };
+// // console.log(information);
+// console.log(information[1]);
+// // console.log(information.comment);
+// information.content ="this is my kingdom";
+// console.log(information.content);
+// information.reposted = 10;
+// console.log(information.reposted);
+
+// object of objects
+
+// const obj ={
+    // Aman : {
+    //     city : "Kanpur",
+    //     positio : "Manager"
+    // },
+    // Amandeep : {
+    //     city : "punjab",
+    //     positio : "SDE"
+    // },
+    // Amaan : {
+    //     city : "aligarh",
+    //     positio : "peon"
+    // }
+// };
+// console.log(obj);
+
+//Array of objects
+
+// const obj =[
+//     {
+//         city : "Kanpur",
+//         position : "Manager"
+//     },
+//     {
+//         city : "punjab",
+//         position : "SDE"
+//     },
+//     {
+//         city : "aligarh",
+//         position : "peon"
+//     }
+// ];
+// console.log(obj[0].city);
+
+//MAth OBjects
+
+console.log(Math.PI);
+console.log(Math.ceil(6.99999999));
+let num =Math.random();
+num = num*5;
+let numnew =Math.floor(num) +20;
+console.log(numnew)
